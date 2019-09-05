4446bb0443f11cb6bcca397b9724f821d9563322
junaidkhan <junaid851@gmail.com>
2019-09-06 01:35:11 +0500

    added viewAny

--- src/Auth/Access/ExtendedAuthorizesRequests.php
+++ src/Auth/Access/ExtendedAuthorizesRequests.php
@@ -16,59 +16,63 @@
     trait ExtendedAuthorizesRequests
     {
         use AuthorizesRequests;
+
         /**
          * Authorize a resource action based on the incoming request.
          *
-         * @param  string  $model
-         * @param  string|null  $parameter
-         * @param  array  $options
-         * @param  \Illuminate\Http\Request|null  $request
-         * @param  array $extendedResourceAbilities
-         * @param  array $extendedResourceMethodsWithoutModels
+         * @param  string                        $model
+         * @param  string|null                   $parameter
+         * @param  array                         $options
+         * @param  \Illuminate\Http\Request|null $request
+         * @param  array                         $extendedResourceAbilities
+         * @param  array                         $extendedResourceMethodsWithoutModels
          * @return void
          */
-        public function authorizeResource($model, $parameter = null, array $options = [], $request = null, array $extendedResourceAbilities = [], array $extendedResourceMethodsWithoutModels = [])
+        public function authorizeResource ( $model, $parameter = null, array $options = [], $request = null, array $extendedResourceAbilities = [], array $extendedResourceMethodsWithoutModels = [] )
         {
-            $parameter = $parameter ?: Str::snake(class_basename($model));
+            $parameter = $parameter ? : Str::snake ( class_basename ( $model ) );
 
             $middleware = [];
 
-            foreach ($this->resourceAbilityMap(!empty($extendedResourceAbilities) ? $extendedResourceAbilities : property_exists ( $model, 'extendedResourceAbilities') ? $model::$extendedResourceAbilities : $extendedResourceAbilities) as $method => $ability) {
-                $modelName = in_array($method, $this->resourceMethodsWithoutModels(!empty($extendedResourceMethodsWithoutModels) ? $extendedResourceMethodsWithoutModels : property_exists ( $model, 'extendedResourceMethodsWithoutModels') ? $model::$extendedResourceMethodsWithoutModels : $extendedResourceMethodsWithoutModels)) ? $model : $parameter;
+            foreach ( $this->resourceAbilityMap ( !empty( $extendedResourceAbilities ) ? $extendedResourceAbilities : property_exists ( $model, 'extendedResourceAbilities' ) ? $model::$extendedResourceAbilities : $extendedResourceAbilities ) as $method => $ability ) {
+                $modelName = in_array ( $method, $this->resourceMethodsWithoutModels ( !empty( $extendedResourceMethodsWithoutModels ) ? $extendedResourceMethodsWithoutModels : property_exists ( $model, 'extendedResourceMethodsWithoutModels' ) ? $model::$extendedResourceMethodsWithoutModels : $extendedResourceMethodsWithoutModels ) ) ? $model : $parameter;
 
-                $middleware["can:{$ability},{$modelName}"][] = $method;
+                $middleware[ "can:{$ability},{$modelName}" ][] = $method;
             }
 
-            foreach ($middleware as $middlewareName => $methods) {
-                $this->middleware($middlewareName, $options)->only($methods);
+            foreach ( $middleware as $middlewareName => $methods ) {
+                $this->middleware ( $middlewareName, $options )->only ( $methods );
             }
         }
 
         /**
          * Get the map of resource methods to ability names.
+         *
          * @param array $extendedResourceAbilities
          * @return array
          */
-        protected function resourceAbilityMap(array $extendedResourceAbilities)
+        protected function resourceAbilityMap ( array $extendedResourceAbilities )
         {
             return array_merge ( [
-                                     'show' => 'view',
-                                     'create' => 'create',
-                                     'store' => 'create',
-                                     'edit' => 'update',
-                                     'update' => 'update',
+                                     'index'   => 'viewAny',
+                                     'show'    => 'view',
+                                     'create'  => 'create',
+                                     'store'   => 'create',
+                                     'edit'    => 'update',
+                                     'update'  => 'update',
                                      'destroy' => 'delete',
-                                 ],$extendedResourceAbilities
+                                 ], $extendedResourceAbilities
             );
         }
 
         /**
          * Get the list of resource methods which do not have model parameters.
+         *
          * @param array $extendedResourceMethodsWithoutModels
          * @return array
          */
-        protected function resourceMethodsWithoutModels(array $extendedResourceMethodsWithoutModels)
+        protected function resourceMethodsWithoutModels ( array $extendedResourceMethodsWithoutModels )
         {
-            return array_merge ( ['index', 'create', 'store'],$extendedResourceMethodsWithoutModels);
+            return array_merge ( [ 'index', 'create', 'store' ], $extendedResourceMethodsWithoutModels );
         }
     }
